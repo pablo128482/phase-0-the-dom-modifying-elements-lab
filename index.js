c959cfba445f9df303bc69292f4dc0be60d01abf
pablo128482 <ashley.c.clay@gmail.com>
2023-06-08 17:30:49 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+    const mainElement = document.querySelector('#main');
+    mainElement.remove();
+    const newHeader = document.createElement("h1");  
+    newHeader.textContent = "P.Diddy is the champion";
+    newHeader.id = "victory";
